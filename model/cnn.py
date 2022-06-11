739fa7394815070ae87f69ed371bcf00ff1bc302
arghya17 <arghyadeepsaha17@gmail.com>
2022-06-11 12:38:50 +0530

    2 block VGG model implemented

--- model/cnn.py
+++ model/cnn.py
@@ -10,9 +10,13 @@ def define_model():
     model=Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
     model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
     model.add(Dense(8, activation='softmax'))
+    #output layer must contain the same number of neurons as the number of classes
+    #8 here since 8 categories are present
 	# compile model
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
@@ -39,7 +43,7 @@ def run_test_harness():
     datagen=ImageDataGenerator(rescale=1.0/255.0)
     train_it=datagen.flow_from_directory('dataset/train',class_mode='categorical',batch_size=32,target_size=(200,200))
     test_it=datagen.flow_from_directory('dataset/test',class_mode='categorical',batch_size=32,target_size=(200,200))
-    history=model.fit(train_it,steps_per_epoch=len(train_it), validation_data=test_it,validation_steps=len(test_it),epochs=20)
+    history=model.fit(train_it,steps_per_epoch=len(train_it), validation_data=test_it,validation_steps=len(test_it),epochs=10)
     _,acc=model.evaluate(test_it,steps=len(test_it))
     print('> %.3f' % (acc * 100.0))
     #learning curve
